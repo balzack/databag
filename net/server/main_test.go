000d98d9345c9e9e424293252114feffb73b9181
Roland Osborne <rolandosborne@gmail.com>
2022-01-11 23:16:08 -0800

    adding tests

--- net/server/main_test.go
+++ net/server/main_test.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+  "fmt"
   "testing"
   "net/http/httptest"
+  "encoding/base64"
   app "databag/internal"
   store "databag/internal/store"
 )
 
-func TestClaim(t *testing.T) {
+func TestSetup(t *testing.T) {
 
   store.SetPath("file::memory:?cache=shared");
+  Claimable(t);
+  Claim(t);
+}
 
+func Claimable(t *testing.T) {
   r := httptest.NewRequest("GET", "/claimable", nil)
   w := httptest.NewRecorder()
   app.GetNodeClaimable(w, r);
@@ -19,3 +25,14 @@ func TestClaim(t *testing.T) {
   }
 }
 
+func Claim(t *testing.T) {
+  auth := base64.StdEncoding.EncodeToString([]byte("admin:pass"))
+  r := httptest.NewRequest("GET", "/claimable", nil)
+  r.Header.Add("Authorization","Basic " + auth)
+  w := httptest.NewRecorder()
+  app.GetNodeClaimable(w, r);
+  if w.Code != 200 {
+    t.Errorf("server not initially claimable");
+  }
+
+}
