1b10c82ecc4e07a00bd82800cb2dd605d1dddc3a
Roland Osborne <rolandosborne@gmail.com>
2022-01-15 13:34:10 -0800

    updating code layout

--- net/server/internal/models.go
+++ net/server/internal/models.go
@@ -1,5 +1,9 @@
 package databag
 
+import (
+	"os"
+)
+
 type Account struct {
 	AccountId string `json:"accountId"`
 	Profile *Profile `json:"profile"`
@@ -12,4 +16,50 @@ type AccountStatus struct {
 	StorageAvailable float64 `json:"storageAvailable"`
 	ForwardingAddress string `json:"forwardingAddress"`
 }
+
+type AccountsImportBody struct {
+	FileName **os.File `json:"fileName,omitempty"`
+}
+
+type Announce struct {
+	AppToken string `json:"appToken"`
+}
+
+type App struct {
+	AppId string `json:"appId"`
+	AppData *AppData `json:"appData"`
+	Attached int32 `json:"attached"`
+}
+
+type AppData struct {
+	Name string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+	Url string `json:"url,omitempty"`
+	Image string `json:"image,omitempty"`
+}
+
+type Article struct {
+	ArticleId string `json:"articleId"`
+	ArticleRevision int64 `json:"articleRevision"`
+	Type_ string `json:"type"`
+	Data string `json:"data"`
+	Created int32 `json:"created"`
+	Modified int32 `json:"modified"`
+	Status string `json:"status"`
+	Labels []string `json:"labels"`
+	Groups []string `json:"groups,omitempty"`
+	TagCount int32 `json:"tagCount"`
+	TagUpdate int32 `json:"tagUpdate,omitempty"`
+	TagRevision int64 `json:"tagRevision"`
+}
+
+type ArticleIdAssetsBody struct {
+	FileName **os.File `json:"fileName,omitempty"`
+}
+
+type ArticleIdSubjectBody struct {
+	Type_ string `json:"type"`
+	Data string `json:"data"`
+}
+
 
