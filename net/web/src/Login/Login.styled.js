261eb657a57b0ff2d65e9e0920baff491e4b88bb
Roland Osborne <rolandosborne@gmail.com>
2022-03-16 10:45:06 -0700

    using styled-components

---
+++ net/web/src/Login/Login.styled.js
@@ -0,0 +1,61 @@
+import { Input, Button } from 'antd';
+import styled from 'styled-components';
+
+export const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  
+  .container {
+    background-color: #ffffff;
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+    border-radius: 4px;
+    max-width: 500px;
+    width: 50%;
+  }
+
+  .header {
+    text-align: center;
+    font-size: 2em;
+    font-weight: bold;
+    color: #555555
+  }
+
+  .subheader {
+    font-size: 0.8em;
+    display: flex;
+    border-bottom: 1px solid black;
+    color: #444444
+    padding-left: 16px
+    padding-right: 16px;
+  }
+
+  .subheader-text {
+    text-align: center;
+    width: 100%;
+  }
+
+`;
+
+export const LoginInput = styled(Input)`
+  margin-top: 16px;
+`;
+
+export const LoginPassword = styled(Input.Password)`
+  margin-top: 16px;
+`;
+
+export const LoginEnter = styled(Button)`
+  align-self: center;
+  margin-top: 16px;
+  min-width: 128px;
+  width: 33%;
+`;
+
+export const LoginCreate = styled(Button)`
+  margin-top: 4px;
+`;
