c8030d27e6640eecf2d106f37baefaab139c77a6
Roland Osborne <rolandosborne@gmail.com>
2022-05-29 02:48:56 -0700

    using mp3 as it seems to have better browser support

--- net/server/transform/transform_vhd.sh
+++ net/server/transform/transform_vhd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -i $1 -y -f mp4 -map_metadata -1 -vf scale=720:-2 -c:v libx264 -crf 23 -preset veryfast -c:a aac $2
+ffmpeg -i $1 -y -f mp4 -map_metadata -1 -vf scale=720:-2 -c:v libx264 -crf 23 -preset veryfast -c:a mp3 $2
--- net/server/transform/transform_vsd.sh
+++ net/server/transform/transform_vsd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -i $1 -y -f mp4 -map_metadata -1 -vf scale=640:-2 -vcodec libx265 -crf 32 -preset veryfast -tag:v hvc1 -acodec aac $2
+ffmpeg -i $1 -y -f mp4 -map_metadata -1 -vf scale=640:-2 -vcodec libx265 -crf 32 -preset veryfast -tag:v hvc1 -acodec mp3 $2
