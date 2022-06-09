2dfb013b656abf3644174f30d14cfa9e33466b15
Roland Osborne <rolandosborne@gmail.com>
2022-06-08 20:50:20 -0700

    renaming entrypoint scropt

--- net/container/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "bind 'set mark-symlinked-directories on'" >> /root/.bashrc
-
-while [ true ]; do
-  sleep 1;
-done
---
+++ net/container/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+cd /app/databag/net/server
+/usr/local/go/bin/go run databag
