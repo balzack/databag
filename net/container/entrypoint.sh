7d2d99cc4f461de27a5063481e8058b14c27e800
Roland Osborne <roland.osborne@gmail.com>
2022-06-12 21:42:29 -0700

    fix, making entrypoint rerunable

--- net/container/entrypoint.sh
+++ net/container/entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 sqlite3 /var/lib/databag/databag.db "VACUUM;"
-sqlite3 /var/lib/databag/databag.db "CREATE TABLE 'configs' ('id' integer NOT NULL UNIQUE,'config_id' text NOT NULL,'str_value' text,'num_value' integer,'bool_value' numeric,'bin_value' blob,PRIMARY KEY ('id'));"
-sqlite3 /var/lib/databag/databag.db "CREATE UNIQUE INDEX 'idx_configs_config_id' ON 'configs'('config_id');"
+sqlite3 /var/lib/databag/databag.db "CREATE TABLE IF NOT EXISTS 'configs' ('id' integer NOT NULL UNIQUE,'config_id' text NOT NULL,'str_value' text,'num_value' integer,'bool_value' numeric,'bin_value' blob,PRIMARY KEY ('id'));"
+sqlite3 /var/lib/databag/databag.db "CREATE UNIQUE INDEX IF NOT EXISTS 'idx_configs_config_id' ON 'configs'('config_id');"
 sqlite3 /var/lib/databag/databag.db "delete from configs where config_id='asset_path';"
 sqlite3 /var/lib/databag/databag.db "delete from configs where config_id='script_path';"
 sqlite3 /var/lib/databag/databag.db "insert into configs (config_id, str_value) values ('asset_path', '/var/lib/databag/');"
