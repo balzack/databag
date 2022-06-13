2301c233fe2f91141531d4bc0bece7e45d1b1acc
Roland Osborne <roland.osborne@gmail.com>
2022-06-12 22:25:47 -0700

    adding delete to cors

--- net/server/main.go
+++ net/server/main.go
@@ -17,7 +17,7 @@ func main() {
 	router := app.NewRouter()
 
   origins := handlers.AllowedOrigins([]string{"*"})
-  methods := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+  methods := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"})
 
   log.Fatal(http.ListenAndServe(":7000", handlers.CORS(origins, methods)(router)))
 }
