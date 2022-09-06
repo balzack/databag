625020f17d652ba132ac1ed26037047df7991ae5
Roland Osborne <roland.osborne@gmail.com>
2022-09-06 14:38:53 -0700

    preparing mobile app

--- app/mobile/App.js
+++ app/mobile/App.js
@@ -1,51 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Button,
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-const App: () => Node = () => {
-
-  const test = () => {
-    console.log("PRESSED: 7000");
-    var ws = new WebSocket('ws://192.168.13.233:7000/status');
-    ws.onerror = (e) => {
-      console.log("error: ", e);
-    }
-    ws.onmessage = (e) => {
-      console.log("message: ", e);
-    }
-    ws.onclose = (e) => {
-      console.log("close: ", e);
-    }
-    ws.onopen = (e) => {
-      console.log("connected");
-      ws.send('something');
-    }
-  }
-
+export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Button title="TEST" onPress={test} />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
