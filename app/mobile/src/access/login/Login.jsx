74f5b3d54b7be576eea69ff0e301fe8074f146b7
Roland Osborne <roland.osborne@gmail.com>
2022-09-07 15:07:22 -0700

    laying out login screen

--- app/mobile/src/access/login/Login.jsx
+++ app/mobile/src/access/login/Login.jsx
@@ -1,11 +1,18 @@
-import { Wrapper, Container } from './Login.styled';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Text } from 'react-native';
+import { Wrapper, Container, Control, Title, Spacer, Header } from './Login.styled';
 
 export function Login() {
   return (
     <Wrapper>
       <Container>
-        <Text>LOGIN</Text>
+        <Control>
+          <Ionicons name="md-cog" size={32} color="grey" />
+        </Control>
+        <Title>Databag</Title>
+        <Spacer>
+          <Header>Login</Header>
+        </Spacer>
       </Container>
     </Wrapper>
   );
