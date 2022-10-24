8340290fabac4e7ffc7135638a8df9ac4c6a3713
Roland Osborne <roland.osborne@gmail.com>
2022-10-24 11:03:14 -0700

    adding optional filter for account listing

--- net/server/internal/api_getAccountListing.go
+++ net/server/internal/api_getAccountListing.go
@@ -8,11 +8,21 @@ import (
 //GetAccountListing retrieves profile list of publicly accessible accounts
 func GetAccountListing(w http.ResponseWriter, r *http.Request) {
 
+  filter := r.FormValue("filter")
 	var accounts []store.Account
-	if err := store.DB.Preload("AccountDetail").Where("searchable = ? AND disabled = ?", true, false).Find(&accounts).Error; err != nil {
-		ErrResponse(w, http.StatusInternalServerError, err)
-		return
-	}
+  if filter == "" {
+    if err := store.DB.Order("id desc").Limit(16).Preload("AccountDetail").Where("searchable = ? AND disabled = ?", true, false).Find(&accounts).Error; err != nil {
+      ErrResponse(w, http.StatusInternalServerError, err)
+      return
+    }
+  } else {
+      username := "%" + filter + "%"
+      PrintMsg(username);
+    if err := store.DB.Order("id desc").Limit(16).Preload("AccountDetail").Where("username LIKE ? AND searchable = ? AND disabled = ?", username, true, false).Find(&accounts).Error; err != nil {
+      ErrResponse(w, http.StatusInternalServerError, err)
+      return
+    }
+  }
 
 	profiles := []CardProfile{}
 	for _, account := range accounts {
