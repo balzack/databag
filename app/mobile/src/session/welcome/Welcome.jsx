0785a3f501d1945173062a8f6f853ec420729bc1
Roland Osborne <roland.osborne@gmail.com>
2022-10-31 16:00:16 -0700

    app base image

--- app/mobile/src/session/welcome/Welcome.jsx
+++ app/mobile/src/session/welcome/Welcome.jsx
@@ -9,12 +9,9 @@ export function Welcome() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.header}>Welcome to Databag</Text>
+      <Text style={styles.header}>Databag</Text>
       <Text style={styles.label}>Communication for the decentralized web</Text>
       <Image style={styles.image} source={session} />
-      <Text style={styles.message}>Step 1: setup your profile</Text>
-      <Text style={styles.message}>Step 2: connect with people</Text>
-      <Text style={styles.message}>Step 3: start a conversation topic</Text>
     </SafeAreaView>
   );
 }
