77c11b9868dddbba8a8d0f45414c87c1babb4d7e
Roland Osborne <roland.osborne@gmail.com>
2022-11-16 10:10:50 -0800

    adding optional server params

--- net/server/main.go
+++ net/server/main.go
@@ -6,18 +6,30 @@ import (
 	"github.com/gorilla/handlers"
 	"log"
 	"net/http"
+  "os"
 )
 
 func main() {
 
 	store.SetPath("/var/lib/databag/databag.db")
 
-	log.Printf("Server started")
-
 	router := app.NewRouter()
 
 	origins := handlers.AllowedOrigins([]string{"*"})
 	methods := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"})
 
-	log.Fatal(http.ListenAndServe(":7000", handlers.CORS(origins, methods)(router)))
+  args := os.Args
+  if len(args) == 3 {
+    port := ":" + args[2]
+    path := "etc/letsencrypt/live/" + args[1]
+    log.Printf("starting server at: " + path + " " + port);
+    log.Fatal(http.ListenAndServeTLS(port, path + "/fullchain.pem", path + "/privkey.pem", handlers.CORS(origins, methods)(router)))
+  } else if len(args) == 2 {
+    path := "etc/letsencrypt/live/" + args[1]
+    log.Printf("starting server at: " + path);
+    log.Fatal(http.ListenAndServeTLS(":443", path + "/fullchain.pem", path + "/privkey.pem", handlers.CORS(origins, methods)(router)))
+  } else {
+    log.Printf("starting server");
+    log.Fatal(http.ListenAndServe(":7000", handlers.CORS(origins, methods)(router)))
+  }
 }
