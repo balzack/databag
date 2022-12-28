67f905ddd42c772fea071b7c289fb5f3cf506852
balzack <roland.osborne@gmail.com>
2022-12-28 15:31:54 -0800

    fixing login with reset token

--- app/mobile/src/api/setAccountAccess.js
+++ app/mobile/src/api/setAccountAccess.js
@@ -1,6 +1,6 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
-export async function setAccountAccess(server, token, appName, appVersion, platofrm, notifications) {
+export async function setAccountAccess(server, token, appName, appVersion, platform, deviceToken, notifications) {
   let access = await fetchWithTimeout(`https://${server}/account/access?token=${token}&appName=${appName}&appVersion=${appVersion}&platform=${platform}&deviceToken=${deviceToken}`, { method: 'PUT', body: JSON.stringify(notifications) })
   checkResponse(access)
   return await access.json()
