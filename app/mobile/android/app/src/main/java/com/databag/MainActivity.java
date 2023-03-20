016154bdf1e7c28fd3153ebe7e549c1ce79c5f38
Roland Osborne <roland.osborne@gmail.com>
2023-03-20 05:26:48 -0700

    apply suggested fix for activity restore crash

--- app/mobile/android/app/src/main/java/com/databag/MainActivity.java
+++ app/mobile/android/app/src/main/java/com/databag/MainActivity.java
@@ -16,6 +16,11 @@ public class MainActivity extends ReactActivity {
     return "Databag";
   }
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
