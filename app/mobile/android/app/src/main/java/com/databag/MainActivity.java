46e76f7b7606d1ed6fe9f58459972dc49df95522
balzack <roland.osborne@gmail.com>
2023-03-20 10:04:59 -0700

    added missing import

--- app/mobile/android/app/src/main/java/com/databag/MainActivity.java
+++ app/mobile/android/app/src/main/java/com/databag/MainActivity.java
@@ -1,5 +1,6 @@
 package com.databag;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
