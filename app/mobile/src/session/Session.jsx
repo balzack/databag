9625ad64e3bdcd0b633ad160b13b28e1ef411812
balzack <roland.osborne@gmail.com>
2023-03-30 22:43:48 -0700

    using keep awake while call active

--- app/mobile/src/session/Session.jsx
+++ app/mobile/src/session/Session.jsx
@@ -1,4 +1,4 @@
-import { useWindowDimensions, View, ScrollView, TouchableOpacity, StatusBar, Text, Image, Modal } from 'react-native';
+import { View, ScrollView, TouchableOpacity, StatusBar, Text, Image, Modal } from 'react-native';
 import { useState, useEffect, useContext } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -24,8 +24,8 @@ import { ProfileContext } from 'context/ProfileContext';
 import { ProfileIcon } from './profileIcon/ProfileIcon';
 import { CardsIcon } from './cardsIcon/CardsIcon';
 import { Logo } from 'utils/Logo';
+import { Call } from './call/Call';
 import splash from 'images/session.png';
-import { RTCView } from 'react-native-webrtc';
 
 const ConversationStack = createStackNavigator();
 const ProfileStack = createStackNavigator();
@@ -164,33 +164,8 @@ export function Session() {
 
   const [ringing, setRinging] = useState([]);
   const { state, actions } = useSession();
-  const {height, width} = useWindowDimensions();
-  const [callWidth, setCallWidth] = useState(0);
-  const [callHeight, setCallHeight] = useState(0);
 
   const drawerParams = { drawerPosition: 'right', headerShown: false, swipeEnabled: false, drawerType: 'front' };
-
-  useEffect(() => {
-    console.log(width, height);
-    if (width > height) {
-      setCallWidth((height * 9)/10);
-      setCallHeight((height * 9)/10);
-    }
-    else {
-      setCallWidth((width * 9)/10);
-      setCallHeight((width * 9)/10);
-    }
-  }, [width, height]);
-
-  useEffect(() => {
-    console.log("**** REMOTE ****");
-    console.log(state.remoteStream);
-  }, [state.remoteStream]);
-
-  useEffect(() => {
-    console.log("**** LOCAL ****");
-    console.log(state.localStream, state.localVideo);
-  }, [state.localStream, state.localVideo]);
 
   const HomeScreen = ({ navParams }) => {
 
@@ -451,59 +426,8 @@ export function Session() {
         visible={state.callStatus != null}
         supportedOrientations={['portrait', 'landscape']}
       >
-        <View style={styles.callBase}>
-          <View style={{ ...styles.callFrame, width: callWidth, height: callHeight }}>
-            { state.remoteStream && (
-              <RTCView
-                style={styles.callRemote}
-                mirror={true}
-                objectFit={'contain'}
-                streamURL={state.remoteStream.toURL()}
-                zOrder={1}
-              />
-            )}
-            { !state.remoteVideo && (
-              <View style={styles.callLogo}>
-                <Logo src={state.callLogo} width={callWidth} height={callHeight} radius={4} />
-              </View>
-            )}
-            { state.localStream && (
-              <RTCView
-                style={styles.callLocal}
-                mirror={true}
-                objectFit={'contain'}
-                streamURL={state.localStream.toURL()}
-                zOrder={2}
-              />
-            )}
-            <View style={styles.callOptions}>
-              { state.localVideo && (
-                <TouchableOpacity style={styles.callOption} onPress={actions.disableVideo}>
-                  <MatIcons name={'video-outline'} size={20} color={Colors.white} />
-                </TouchableOpacity>
-              )}
-              { !state.localVideo && (
-                <TouchableOpacity style={styles.callOption} onPress={actions.enableVideo}>
-                  <MatIcons name={'video-off-outline'} size={20} color={Colors.white} />
-                </TouchableOpacity>
-              )}
-              { state.localAudio && (
-                <TouchableOpacity style={styles.callOption} onPress={actions.disableAudio}>
-                  <MatIcons name={'microphone'} size={20} color={Colors.white} />
-                </TouchableOpacity>
-              )}
-              { !state.localAudio && (
-                <TouchableOpacity style={styles.callOption} onPress={actions.enableAudio}>
-                  <MatIcons name={'microphone-off'} size={20} color={Colors.white} />
-                </TouchableOpacity>
-              )}
-            </View>
-            <TouchableOpacity style={styles.callEnd} onPress={actions.end}>
-              <MatIcons name={'phone-hangup'} size={20} color={Colors.white} />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
+        <Call />
+     </Modal>
     </NavigationContainer>
   );
 }
