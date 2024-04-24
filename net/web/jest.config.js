c52d54b0377bb56464a6ee0e941b8a3c4c66d7e9
Lucian I. Last <li@last.nl>
2024-04-24 09:53:34 +0200

    lint new files

--- net/web/jest.config.js
+++ net/web/jest.config.js
@@ -2,15 +2,11 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  modulePaths: [
-        "<rootDir>/src/"
-      ],
-      testMatch: [
-        "**/test/**"
-      ],
-      moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/mock/fileMock.js",
-        "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-        // "(.*)": "<rootDir>/src/$1",
-      }
+  modulePaths: ['<rootDir>/src/'],
+  testMatch: ['**/test/**'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/mock/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    // "(.*)": "<rootDir>/src/$1",
+  },
 };
