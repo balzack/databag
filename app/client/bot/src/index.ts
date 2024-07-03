d03399f24f8a673dc8099572546f964730c27c87
balzack <roland.osborne@gmail.com>
2024-07-02 19:33:20 -0700

    linking sample bot to sdk

--- app/client/bot/src/index.ts
+++ app/client/bot/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Databag Bot")
+import { DatabagSDK } from 'databag-client-sdk';
+
+const run = async () => {
+  const sdk = new DatabagSDK(null);
+  const bot = await sdk.automate();
+  console.log(bot);
+}
+
+run();
