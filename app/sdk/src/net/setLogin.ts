42c9546efdbc1ae3457c0764796623c986ae20df
balzack <roland.osborne@gmail.com>
2024-07-12 16:51:53 -0700

    adding endpoint request

---
+++ app/sdk/src/net/setLogin.ts
@@ -0,0 +1,14 @@
+import axios from 'redaxios';
+import { encode } from './base64';
+
+export async function setLogin(url: string, username: string, password: string, code: string | null, appName: string, appVersion: string, platform: string, deviceToken: string, pushType: string, notifications: { event: string, messageTitle: string}[]): Promise<{ guid: string, appToken: string, created: number, pushSupported: boolean }> {
+  const mfa = code ? `$code=${code}` : '';
+  const endpoint = `${url}/account/apps?appName=${appName}&appVersion=${appVersion}&platform=${platform}&deviceToken=${deviceToken}&pushType=${pushType}${mfa}`;
+  const auth = encode(`${username}:${password}`);
+  const response = await axios.post(endpoint, notifications, { auth: `Basic ${auth}` });
+  if (response.status >= 400 && response.status < 600) {
+    throw new Error('setLogin fetch failed');
+  }
+
+  return response.data;
+}
