098f095b42b4205e78e07a5792b40b524359ef57
balzack <roland.osborne@gmail.com>
2024-08-11 09:13:10 +0200

    limiting height for login form

--- app/client/web/src/access/Access.tsx
+++ app/client/web/src/access/Access.tsx
@@ -36,113 +36,82 @@ export function Access() {
       )}
       {state.display != null && (
         <div className={classes.right}>
-          {state.mode !== 'admin' && (
-            <Button
-              variant="transparent"
-              className={classes.float}
-              leftSection={<IconSettings size={28} />}
-              onClick={() => actions.setMode('admin')}
-            />
-          )}
-          {state.mode === 'admin' && (
-            <Button
-              variant="transparent"
-              className={classes.float}
-              leftSection={<IconUser size={28} />}
-              onClick={() => actions.setMode('login')}
-            />
-          )}
-          <Title className={classes.title} order={1}>
-            Databag
-          </Title>
-          {state.mode === 'login' && (
-            <>
-              <Title order={3}>{state.strings.login}</Title>
-              <Space h="md" />
-              <Button size="compact-sm" variant="transparent" onClick={open}>
-                {state.hostname}
-              </Button>
-              <TextInput
-                className={classes.input}
-                size="md"
-                leftSectionPointerEvents="none"
-                leftSection={<IconUser />}
-                placeholder={state.strings.username}
-                onChange={(event) =>
-                  actions.setUsername(event.currentTarget.value)
-                }
-              />
-              <PasswordInput
-                className={classes.input}
-                size="md"
-                leftSection={<IconLock />}
-                placeholder={state.strings.password}
-                onChange={(event) =>
-                  actions.setPassword(event.currentTarget.value)
-                }
-              />
-              <Space h="md" />
-              <Button variant="filled" className={classes.submit}>
-                {state.strings.login}
-              </Button>
+          <div className={classes.frame}>
+            {state.mode !== 'admin' && (
               <Button
-                size="compact-sm"
-                variant="subtle"
-                onClick={() => actions.setMode('create')}
-              >
-                {state.strings.createAccount}
-              </Button>
-              <Button
-                size="compact-sm"
-                variant="subtle"
-                onClick={() => actions.setMode('access')}
-              >
-                {state.strings.forgotPassword}
-              </Button>
-            </>
-          )}
-          {state.mode === 'access' && (
-            <>
-              <Title order={3}>{state.strings.accessAccount}</Title>
-              <Space h="md" />
-              <Button size="compact-sm" variant="transparent" onClick={open}>
-                {state.hostname}
-              </Button>
-              <TextInput
-                className={classes.input}
-                size="md"
-                leftSectionPointerEvents="none"
-                leftSection={<IconKey />}
-                placeholder={state.strings.accessCode}
-                onChange={(event) =>
-                  actions.setToken(event.currentTarget.value)
-                }
+                variant="transparent"
+                className={classes.float}
+                leftSection={<IconSettings size={28} />}
+                onClick={() => actions.setMode('admin')}
               />
-              <Space h="md" />
-              <Button variant="filled" className={classes.submit}>
-                {state.strings.login}
-              </Button>
+            )}
+            {state.mode === 'admin' && (
               <Button
-                size="compact-sm"
-                variant="subtle"
+                variant="transparent"
+                className={classes.float}
+                leftSection={<IconUser size={28} />}
                 onClick={() => actions.setMode('login')}
-              >
-                {state.strings.accountLogin}
-              </Button>
-            </>
-          )}
-          {state.mode === 'create' && (
-            <>
-              <Title order={3}>{state.strings.createAccount}</Title>
-              <Space h="md" />
-              <Button size="compact-sm" variant="transparent" onClick={open}>
-                {state.hostname}
-              </Button>
-              {(state.available === 0 || !state.availableSet) && (
+              />
+            )}
+            <Title className={classes.title} order={1}>
+              Databag
+            </Title>
+            {state.mode === 'login' && (
+              <>
+                <Title order={3}>{state.strings.login}</Title>
+                <Space h="md" />
+                <Button size="compact-sm" variant="transparent" onClick={open}>
+                  {state.hostname}
+                </Button>
                 <TextInput
                   className={classes.input}
                   size="md"
-                  disabled={!state.availableSet}
+                  leftSectionPointerEvents="none"
+                  leftSection={<IconUser />}
+                  placeholder={state.strings.username}
+                  onChange={(event) =>
+                    actions.setUsername(event.currentTarget.value)
+                  }
+                />
+                <PasswordInput
+                  className={classes.input}
+                  size="md"
+                  leftSection={<IconLock />}
+                  placeholder={state.strings.password}
+                  onChange={(event) =>
+                    actions.setPassword(event.currentTarget.value)
+                  }
+                />
+                <Space h="md" />
+                <Button variant="filled" className={classes.submit}>
+                  {state.strings.login}
+                </Button>
+                <Button
+                  size="compact-sm"
+                  variant="subtle"
+                  onClick={() => actions.setMode('create')}
+                >
+                  {state.strings.createAccount}
+                </Button>
+                <Button
+                  size="compact-sm"
+                  variant="subtle"
+                  onClick={() => actions.setMode('access')}
+                >
+                  {state.strings.forgotPassword}
+                </Button>
+              </>
+            )}
+            {state.mode === 'access' && (
+              <>
+                <Title order={3}>{state.strings.accessAccount}</Title>
+                <Space h="md" />
+                <Button size="compact-sm" variant="transparent" onClick={open}>
+                  {state.hostname}
+                </Button>
+                <TextInput
+                  className={classes.input}
+                  size="md"
                   leftSectionPointerEvents="none"
                   leftSection={<IconKey />}
                   placeholder={state.strings.accessCode}
@@ -150,83 +119,116 @@ export function Access() {
                     actions.setToken(event.currentTarget.value)
                   }
                 />
-              )}
-              <TextInput
-                className={classes.input}
-                size="md"
-                leftSectionPointerEvents="none"
-                leftSection={<IconUser />}
-                placeholder={state.strings.username}
-                onChange={(event) =>
-                  actions.setUsername(event.currentTarget.value)
-                }
+                <Space h="md" />
+                <Button variant="filled" className={classes.submit}>
+                  {state.strings.login}
+                </Button>
+                <Button
+                  size="compact-sm"
+                  variant="subtle"
+                  onClick={() => actions.setMode('login')}
+                >
+                  {state.strings.accountLogin}
+                </Button>
+              </>
+            )}
+            {state.mode === 'create' && (
+              <>
+                <Title order={3}>{state.strings.createAccount}</Title>
+                <Space h="md" />
+                <Button size="compact-sm" variant="transparent" onClick={open}>
+                  {state.hostname}
+                </Button>
+                {(state.available === 0 || !state.availableSet) && (
+                  <TextInput
+                    className={classes.input}
+                    size="md"
+                    disabled={!state.availableSet}
+                    leftSectionPointerEvents="none"
+                    leftSection={<IconKey />}
+                    placeholder={state.strings.accessCode}
+                    onChange={(event) =>
+                      actions.setToken(event.currentTarget.value)
+                    }
+                  />
+                )}
+                <TextInput
+                  className={classes.input}
+                  size="md"
+                  leftSectionPointerEvents="none"
+                  leftSection={<IconUser />}
+                  placeholder={state.strings.username}
+                  onChange={(event) =>
+                    actions.setUsername(event.currentTarget.value)
+                  }
+                />
+                <PasswordInput
+                  className={classes.input}
+                  size="md"
+                  leftSection={<IconLock />}
+                  placeholder={state.strings.password}
+                  onChange={(event) =>
+                    actions.setPassword(event.currentTarget.value)
+                  }
+                />
+                <PasswordInput
+                  className={classes.input}
+                  size="md"
+                  leftSection={<IconLock />}
+                  placeholder={state.strings.confirmPassword}
+                  onChange={(event) =>
+                    actions.setConfirm(event.currentTarget.value)
+                  }
+                />
+                <Space h="md" />
+                <Button variant="filled" className={classes.submit}>
+                  {state.strings.create}
+                </Button>
+                <Button
+                  variant="subtle"
+                  onClick={() => actions.setMode('login')}
+                  size="compact-sm"
+                >
+                  {state.strings.accountLogin}
+                </Button>
+              </>
+            )}
+            {state.mode === 'admin' && (
+              <>
+                <Title order={3}>{state.strings.admin}</Title>
+                <Space h="md" />
+                <Button size="compact-sm" variant="transparent" onClick={open}>
+                  {state.hostname}
+                </Button>
+                <PasswordInput
+                  className={classes.input}
+                  size="md"
+                  leftSection={<IconLock />}
+                  placeholder={state.strings.password}
+                  onChange={(event) =>
+                    actions.setPassword(event.currentTarget.value)
+                  }
+                />
+                <Space h="md" />
+                <Button variant="filled" className={classes.submit}>
+                  {state.strings.login}
+                </Button>
+              </>
+            )}
+            <div className={classes.settings}>
+              <Select
+                label={state.strings.theme}
+                data={state.themes}
+                value={state.theme}
+                onChange={(theme) => actions.setTheme(theme as string)}
               />
-              <PasswordInput
-                className={classes.input}
-                size="md"
-                leftSection={<IconLock />}
-                placeholder={state.strings.password}
-                onChange={(event) =>
-                  actions.setPassword(event.currentTarget.value)
-                }
+              <Select
+                label={state.strings.language}
+                data={state.languages}
+                value={state.language}
+                onChange={(language) => actions.setLanguage(language as string)}
               />
-              <PasswordInput
-                className={classes.input}
-                size="md"
-                leftSection={<IconLock />}
-                placeholder={state.strings.confirmPassword}
-                onChange={(event) =>
-                  actions.setConfirm(event.currentTarget.value)
-                }
-              />
-              <Space h="md" />
-              <Button variant="filled" className={classes.submit}>
-                {state.strings.create}
-              </Button>
-              <Button
-                variant="subtle"
-                onClick={() => actions.setMode('login')}
-                size="compact-sm"
-              >
-                {state.strings.accountLogin}
-              </Button>
-            </>
-          )}
-          {state.mode === 'admin' && (
-            <>
-              <Title order={3}>{state.strings.admin}</Title>
-              <Space h="md" />
-              <Button size="compact-sm" variant="transparent" onClick={open}>
-                {state.hostname}
-              </Button>
-              <PasswordInput
-                className={classes.input}
-                size="md"
-                leftSection={<IconLock />}
-                placeholder={state.strings.password}
-                onChange={(event) =>
-                  actions.setPassword(event.currentTarget.value)
-                }
-              />
-              <Space h="md" />
-              <Button variant="filled" className={classes.submit}>
-                {state.strings.login}
-              </Button>
-            </>
-          )}
-          <div className={classes.settings}>
-            <Select
-              label={state.strings.theme}
-              data={state.themes}
-              value={state.theme}
-              onChange={(theme) => actions.setTheme(theme as string)}
-            />
-            <Select
-              label={state.strings.language}
-              data={state.languages}
-              value={state.language}
-              onChange={(language) => actions.setLanguage(language as string)}
-            />
+            </div>
           </div>
         </div>
       )}
