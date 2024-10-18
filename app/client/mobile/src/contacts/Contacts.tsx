fab9367409ae638352c9d38dec5e155332ca4489
balzack <roland.osborne@gmail.com>
2024-10-17 17:07:48 -0700

    rendering contact list header in mobile

--- app/client/mobile/src/contacts/Contacts.tsx
+++ app/client/mobile/src/contacts/Contacts.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import {Text} from 'react-native-paper';
-import {SafeAreaView} from 'react-native';
+import {Divider, Surface, IconButton, Button, TextInput} from 'react-native-paper';
+import {SafeAreaView, View} from 'react-native';
+import {styles} from './Contacts.styled';
+import {useContacts} from './useContacts.hook';
 
 export function Contacts() {
+  const { state, actions } = useContacts();
+
   return (
-    <SafeAreaView>
-      <Text>Contacts</Text>
+    <SafeAreaView style={styles.contacts}>
+      <View style={styles.header}>
+        <IconButton style={{ borderRadius: 4 }} mode="contained" icon="sort-ascending" size={24} />
+
+        <Surface mode="flat" style={styles.inputSurface}>
+          <TextInput dense={true} style={styles.input} unserlineStyle={styles.inputUnderline} mode="outlined" placeholder={state.strings.contacts} left={<TextInput.Icon style={styles.icon} icon="magnify" />} />
+        </Surface>
+
+        <Button icon="account-plus" mode="contained" style={{ borderRadius: 8 }}>{ state.strings.add }</Button>
+      </View>
+      <Divider style={{ width: '100%', height: 2 }} />
     </SafeAreaView>
   );
 }
+
