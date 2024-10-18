2f8ec42bcf11a75563e19b3da7c36d61ce5f8a45
balzack <roland.osborne@gmail.com>
2024-10-18 12:06:39 -0700

    adjusting sort icon

--- app/client/mobile/src/contacts/Contacts.tsx
+++ app/client/mobile/src/contacts/Contacts.tsx
@@ -11,7 +11,7 @@ export function Contacts() {
   return (
     <SafeAreaView style={styles.contacts}>
       <View style={styles.header}>
-        <IconButton style={{ borderRadius: 4 }} mode="contained" icon={state.sortAsc ? 'sort-ascending' : 'sort-descending'} size={24} onPress={actions.toggleSort} />
+        <IconButton style={{ borderRadius: 4 }} mode="contained" icon={state.sortAsc ? 'sort-descending' : 'sort-ascending'} size={24} onPress={actions.toggleSort} />
 
         <Surface mode="flat" style={styles.inputSurface}>
           <TextInput dense={true} style={styles.input} unserlineStyle={styles.inputUnderline} mode="outlined" placeholder={state.strings.contacts} left={<TextInput.Icon style={styles.icon} icon="magnify" />} value={state.filter} onChangeText={value => actions.setFilter(value)} />
@@ -27,9 +27,6 @@ export function Contacts() {
           data={state.filtered}
           initialNumToRender={32}
           renderItem={({ item }) => {
-
-console.log(">>>>", item);
-
             const call = <IconButton icon="phone" />
             const message = <IconButton icon="message" />
             const options = item.status === 'connected' && !item.offsync ? [message, call] : [];
@@ -39,10 +36,8 @@ console.log(">>>>", item);
               openContact(params);
             }
             const status = item.offsync ? styles.offsync : styles[item.status];
-
             return (
               <Card containerStyle={styles.card} imageUrl={item.imageUrl} name={item.name} handle={item.handle} node={item.node} placeholder={state.strings.name} select={select} actions={options} />
-
             )
           }}
           keyExtractor={(card) => card.cardId}
