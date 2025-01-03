bb04570b2750dac688c1c6352b991cae1edee555
Lucian I. Last <li@last.nl>
2025-01-03 15:14:35 +0100

    Tested example linux on bare metal

--- examples/linux/install.sh
+++ examples/linux/install.sh
@@ -9,24 +9,20 @@ function confirm() {
 	fi
 }
 
-if [ "$USER" != "databag" ]; then
-	echo "Install script must be run as databag user"
-	if id "databag" >/dev/null 2>&1; then
-		echo "User databag not found, creating..."
-		confirm
-		sudo useradd databag
-	fi
-	sudo -i -u databag $0 "$@"
-	exit
+if ! id "databag" >/dev/null 2>&1; then
+	echo "User databag not found, creating..."
+	confirm
+	sudo useradd databag
 fi
 
 if [[ ! -d "/app/databag" ]]; then
 	echo "Creating app directory for databag, this requires sudo permissions"
+	sudo rm -rf /app/databag || true
 	sudo mkdir -p /app/databag
-	sudo chown -R $USER:$USER /app/databag
 
 	echo "Downloading databag repository into /app/databag"
 	git clone --depth 1 https://github.com/balzack/databag.git /app/databag
+	sudo chown -R databag:databag /app/databag
 fi
 cd /app/databag
 
@@ -42,16 +38,18 @@ if [[ "/app/databag" != $(pwd) ]]; then
 	exit 1
 fi
 
-if ![[ -z $(which yarn) ]]; then
+if [[ ! -z $(which yarn) ]]; then
 	echo "Yarn is not installed, installing..."
-	echo "On linux this will require sudo permissions to install"
-	sudo npm install --global yarn
+	npm install --global yarn
 fi
 
 echo "Building frontend files..."
 cd net/web
 yarn --frozen-lockfile
+echo "Removing old frontend files, requires sudo permissions..."
+sudo rm -rf build
 yarn run build
+sudo chown -R databag:databag build
 cd ../..
 
 echo "Building backend files..."
@@ -60,18 +58,19 @@ CGO_ENABLED=1 go build -o databag .
 cd ../..
 
 echo "Creating databag locations..."
-RUN mkdir -p /opt/databag
-RUN mkdir -p /var/lib/databag
+sudo mkdir -p /opt/databag
+sudo mkdir -p /var/lib/databag
+sudo chown -R databag:databag /var/lib/databag
 
 echo "Copying transform scripts..."
-mkdir -p /opt/databag/transform
-cp net/transform/*.sh /opt/databag/transform/
+sudo mkdir -p /opt/databag/transform
+sudo cp net/transform/*.sh /opt/databag/transform/
+sudo chmod +x /opt/databag/transform/*.sh
 
-if ![[ -f /etc/systemd/system/databag.service ]]; then
+if [[ ! -f /etc/systemd/system/databag.service ]]; then
 	function createService() {
-		echo "Creating databag service..."
-		echo "Copying databag service to /etc/systemd/system/databag.service requires sudo permissions"
-		sudo cp ./databag.service /etc/systemd/system/databag.service
+		echo "Creating databag service, requires sudo permissions..."
+		sudo cp examples/linux/databag.service /etc/systemd/system/databag.service
 		sudo chmod 664 /etc/systemd/system/databag.service
 		sudo systemctl daemon-reload
 	}
