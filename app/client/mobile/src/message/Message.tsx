d0e69246cc05a00bdda45833469609dbce8c5a29
balzack <roland.osborne@gmail.com>
2025-03-26 23:15:06 -0700

    fixing url pattern on mobile

--- app/client/mobile/src/message/Message.tsx
+++ app/client/mobile/src/message/Message.tsx
@@ -61,6 +61,7 @@ export function Message({
   useEffect(() => {
     const urlPattern = new RegExp('(https?:\\/\\/)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,4}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)');
     const hostPattern = new RegExp('^https?:\\/\\/', 'i');
+    const dotPattern = new RegExp('^.*\\.\\..*$')
 
     let plain = '';
     let clickable = [];
@@ -69,7 +70,7 @@ export function Message({
     if (parsed?.length > 0) {
       const words = parsed as string[];
       words.forEach((word, index) => {
-        if (urlPattern.test(word)) {
+        if (urlPattern.test(word) && !dotPattern.test(word)) {
           clickable.push(
             <Text key={index} style={textStyle}>
               {plain}
