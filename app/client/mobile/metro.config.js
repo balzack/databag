3176da13b5664c0c7773465ad31acf2e2d02ff33
Roland Osborne <rolandosborne@gmail.com>
2025-06-14 00:29:58 -0700

    fixing more lint

--- app/client/mobile/metro.config.js
+++ app/client/mobile/metro.config.js
@@ -12,13 +12,13 @@ const { assetExts, sourceExts } = defaultConfig.resolver;
 const config = {
   transformer: {
     babelTransformerPath: require.resolve(
-      "react-native-svg-transformer/react-native"
-    )
+      'react-native-svg-transformer/react-native'
+    ),
   },
   resolver: {
-    assetExts: assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...sourceExts, "svg"]
-  }
+    assetExts: assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
 };
 
 module.exports = mergeConfig(defaultConfig, config);
