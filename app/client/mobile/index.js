8ec9b8143b98722046d2fa80ed52d00c6c19c1b2
Roland Osborne <roland.osborne@gmail.com>
2025-07-12 06:19:10 -0700

    ignore exception from background notification

--- app/client/mobile/index.js
+++ app/client/mobile/index.js
@@ -8,6 +8,12 @@ import App from './App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
 
+
+ErrorUtils.setGlobalHandler((error, isFatal) => {
+  console.log('Caught a global error:', error);
+  console.log('Is fatal:', isFatal);
+});
+
 messaging().registerDeviceForRemoteMessages().then(() => { });
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {});
