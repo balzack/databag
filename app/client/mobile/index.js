7cf4f57ea669207b313fe575945f41a3421df55b
Roland Osborne <roland.osborne@gmail.com>
2025-07-16 04:54:03 +0000

    removing dependencies for fdroid

--- app/client/mobile/index.js
+++ app/client/mobile/index.js
@@ -6,16 +6,11 @@ import './gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import messaging from '@react-native-firebase/messaging';
 
 
 ErrorUtils.setGlobalHandler((error, isFatal) => {
   console.log('Caught a global error:', error);
   console.log('Is fatal:', isFatal);
 });
-
-messaging().registerDeviceForRemoteMessages().then(() => { });
-
-messaging().setBackgroundMessageHandler(async remoteMessage => {});
 
 AppRegistry.registerComponent(appName, () => App);
