88159cc0d04358508151eae939964d25b4637d38
Roland Osborne <roland.osborne@gmail.com>
2025-07-16 05:50:30 +0000

    restoring more metro config

--- app/client/mobile/metro.config.js
+++ app/client/mobile/metro.config.js
@@ -1,2 +1,15 @@
-module.exports = {
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
 };
+
+module.exports = mergeConfig(defaultConfig, config);
