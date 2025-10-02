fb710f206b6f8fb40cb1770a93a290537bd49e2b
Roland Osborne <roland.osborne@gmail.com>
2025-10-02 22:20:06 +0000

    adjusting blur

--- app/mobile/src/utils/BlurView.tsx
+++ app/mobile/src/utils/BlurView.tsx
@@ -3,5 +3,5 @@ import { useTheme } from 'react-native-paper';
 
 export function BlurView() {
   const theme = useTheme();
-  return <View style={{ position: 'absolute', width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.7)' }}></View>
+  return <View style={{ position: 'absolute', width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.9)' }}></View>
 }
